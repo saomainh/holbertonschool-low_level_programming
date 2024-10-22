0f585912518dd06b97dd5c789806ab436d9e3c50
saomainh <enora.nh@gmail.com>
2024-10-22 13:03:10 +0000

    1(2)

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ n = rand() - RAND_MAX / 2;
 last_digit = abs(n % 10);
 if (last_digit > 5)
 {
-printf("Last digit of %d is %d and is greater than \n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
