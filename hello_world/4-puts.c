d0b23f77dbcab2ec9e6345fd6c38df2ab175a16e
saomainh <enora.nh@gmail.com>
2024-10-22 07:57:41 +0000

    4(2)

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
-   puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+/**
+ * hello_puts - prints a quote
+ *
+ * Description : prints a single line using puts
+ * Return : 0 if it works
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
-
-
-
-    
-    
-
