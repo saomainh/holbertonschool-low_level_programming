f7f25c69261e60d9d4fa41aff892e92c2136fc7d
saomainh <enora.nh@gmail.com>
2024-10-22 09:09:29 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints size of types
+ *
+ * Description: prints the size of various types depending on the computer it is compiled and run on
+ * Return: 0 if it works
+ */
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+return (0);
+}
---
+++ hello_world/6-size.c~
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a quote
+ *
+ * Description: prints a single line using printf followed by a new line
+ * Return: 0 if it works
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
