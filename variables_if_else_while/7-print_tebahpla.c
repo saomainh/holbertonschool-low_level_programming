4d416e9caab0916653c2370e70c2bdfd40e3827b
saomainh <enora.nh@gmail.com>
2024-10-23 13:14:43 +0000

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - print random number and if positive or negative
+ *
+ *Description : /
+ *Return: 0 if it worked
+ */
+int main(void)
+{
+char ch = 122;
+char space = '\n';
+for (ch = 122; ch >= 97; ch--)
+putchar(ch);
+putchar(space);
+return (0);
+}
