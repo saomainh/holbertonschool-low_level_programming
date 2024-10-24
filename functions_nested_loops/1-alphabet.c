c24b3dbcc0cf1c32c976c7ca39a72bee78ac1bbb
saomainh <enora.nh@gmail.com>
2024-10-24 17:15:21 +0000

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - print random number and if positive or negative
+ *
+ *Description : /
+ *Return: 0 if it worked
+ */
+
+void print_alphabet(void)
+{
+char ch = 97;
+for (ch = 97; ch <= 122; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
