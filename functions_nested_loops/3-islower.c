3cc8f98eed085efe8cda81ded7a2dfb0590ca665
saomainh <enora.nh@gmail.com>
2024-10-24 18:10:09 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - it just prints the alphabet x10
+ *
+ *Description : /
+ *Return: no return
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
