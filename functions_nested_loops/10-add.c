4207ec840534f7f17ea086d4ef681b701f3110ff
saomainh <enora.nh@gmail.com>
2024-10-27 22:58:47 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_last_digit - bsolute value of a number
+ *@n: number  to test
+ *Description : /
+ *Return: absolute value of n
+ */
+
+int add(int, int)
+{
+	int minutes;
+	for (minutes = 0; minutes <= 59; minutes++)
+	{
+		
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+		_putchar('\n');
+	}
+}
