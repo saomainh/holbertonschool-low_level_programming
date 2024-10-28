15ac7fe3c2fae36bf7e996596c70ea16dbb88560
saomainh <enora.nh@gmail.com>
2024-10-28 14:27:58 +0000

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isdigit - it just prints the alphabet
+ *@c : eaofhezui
+ *Description : /
+ *Return: no return
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
