0b781a2bcf9b14e9020a4b7d8df8510d16fc854f
saomainh <enora.nh@gmail.com>
2024-10-28 15:16:00 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -18,12 +18,8 @@ void more_numbers(void)
 			if (b >= 10)
 			{
 				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-		    }
-			else
-			{
-				_putchar(b + '0');
 			}
+		    _putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
