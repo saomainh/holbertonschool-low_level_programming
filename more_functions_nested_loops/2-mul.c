cacd05d91672d6891cb8fc8f8a0d3ee7e7cba683
saomainh <enora.nh@gmail.com>
2024-10-28 14:31:47 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - it just prints the alphabet
+ *@a : eaofhezui
+ *@b : afeafe
+ *Description : /
+ *Return: no return
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}	
