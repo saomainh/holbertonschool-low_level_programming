b76fb4228fceaf23b5a53f5bfce51b8fc95afaf6
saomainh <enora.nh@gmail.com>
2024-10-28 14:25:55 +0000

    0 betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *print_alphabet - it just prints the alphabet
- *
+ *_isupper - it just prints the alphabet
+ *@c : eaofhezui
  *Description : /
  *Return: no return
  */
@@ -10,11 +10,11 @@
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-	{	
+	{
 		return (1);
-	}	
+	}
 	else
 	{
 		return (0);
-	}	
-}	
+	}
+}
