a1afa55cbd0906f248d8a2713b44740b854d0fc8
saomainh <enora.nh@gmail.com>
2024-10-28 13:02:39 +0000

    9(2)

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,13 +17,25 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 	    {
 		    result = a * b;
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
-			if (b < 9)
-		    {
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+			if (result >= 10)
+			{	
+			    _putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			    if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+		    }
+			else
+			{
+				_putchar(result + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+			    }
 		    }
 	    }
 		_putchar('\n');
