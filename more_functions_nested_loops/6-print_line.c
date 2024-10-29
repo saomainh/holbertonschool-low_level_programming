c42735fe060ff3cd7a91cd3409084281af4d3cc5
saomainh <enora.nh@gmail.com>
2024-10-29 13:21:30 +0000

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line - it just prints the alphabet
+ *@n : eofiefoezji
+ *Description : /
+ *Return:
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
