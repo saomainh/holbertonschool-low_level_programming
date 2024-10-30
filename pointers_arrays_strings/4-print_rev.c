d8412f0fd468811a27c3827533c51560705c631c
saomainh <enora.nh@gmail.com>
2024-10-30 12:50:10 +0000

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_rev - it just prints the alphabet
+ *@s : eaofhezui
+ *Description : /
+ *Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int count = 0;
+
+	while (s[i] != s[-1])
+	{
+		count++;
+		i++;
+	}
+	for (i = count; i >= 0 - 1; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void print_rev(char *s);
 void _puts(char *str);
 int _putchar(char c);
 int _strlen(char *s);
