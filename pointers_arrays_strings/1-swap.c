649a1342235f446509f5f50678ab62f9998b27b5
saomainh <enora.nh@gmail.com>
2024-10-30 10:16:10 +0000

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - it just prints the alphabet
+ *@a : eaofhezui
+ *@b : afeaz
+ *Description : /
+ *Return: no return
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
