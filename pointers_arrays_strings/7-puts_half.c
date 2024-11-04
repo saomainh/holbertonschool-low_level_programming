44821b337817f8de0c1a5e078c672ad0b8142813
saomainh <enora.nh@gmail.com>
2024-11-04 20:30:29 +0000

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "stddef.h"
+
+/**
+ *puts2 - it just prints the alphabet
+ *@str : eaofhezui
+ *Description : /
+ *Return: no return
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int count = 0;
+
+	while (str[i] != str[-1])
+	{
+		count++;
+		i++;
+	}
+	for (i = count / 2; i < count; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void puts_half(char *str);
 void puts2(char *str);
 void print_rev(char *s);
 void _puts(char *str);
