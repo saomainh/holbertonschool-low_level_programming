716fbc6971a80460b4897024fdf426ea6f0f7077
saomainh <enora.nh@gmail.com>
2024-11-04 20:35:34 +0000

    7(2)

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,13 +12,22 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int count = 0;
+	int midlenght;
 
 	while (str[i] != str[-1])
 	{
 		count++;
 		i++;
 	}
-	for (i = count / 2; i < count; i++)
+	if (count % 2 != 0)
+	{
+		midlenght = (count - 1) / 2;
+	}
+	else
+	{
+		midlenght = count / 2;
+	}	
+	for (i = midlenght; i < count; i++)
 	{
 		_putchar(str[i]);
 	}
