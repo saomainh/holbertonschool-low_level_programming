6018204a9b913c4b32b7c4bf7f1b8b99550810bd
saomainh <enora.nh@gmail.com>
2024-11-04 21:02:36 +0000

    5 betty2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,11 @@
-void reverse_string(char *str)
+/**
+ * rev_string - Short description, single line
+ * @str: Description of parameter x
+ * Description: Longer description of the function
+ * Return: Description of the returned value
+ */
+
+void rev_string(char *str)
 {
     char *start = str;
     char *end = str;
