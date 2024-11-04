b1c59184294d5419a8bc8806ddc535dcfa16bbeb
saomainh <enora.nh@gmail.com>
2024-11-04 20:59:11 +0000

    5 betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-
-void rev_string(char *str) {
+void reverse_string(char *str)
+{
     char *start = str;
     char *end = str;
 
-
-    while (*end != '\0') {
+    while (*end != '\0')
+    {
         end++;
     }
     end--;
 
-    while (start < end) {
+    while (start < end)
+    {
         char temp = *start;
         *start = *end;
         *end = temp;
