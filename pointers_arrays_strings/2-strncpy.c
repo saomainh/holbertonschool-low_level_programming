0147dae898bce06eacf138f0e9b691c782a95b7a
saomainh <enora.nh@gmail.com>
2024-11-11 21:33:24 +0000

    2 part 2-3

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -37,15 +37,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	/*int og_len = _len(dest);*/
 	
-	for (i = 0; i < n; i++)
-	{
-		/**
-		if (*src == '\0')
-		{
-		    dest[i] = '\0';
-		}
-		**/
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
