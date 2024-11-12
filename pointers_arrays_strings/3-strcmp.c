6f0860ca18ea7d4a9a1074406a743dcff37c7330
saomainh <enora.nh@gmail.com>
2024-11-12 00:32:56 +0000

    3 part 2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,50 +4,17 @@
 #include <stdlib.h>
 
 /**
- *_len - finds the lenght of a string
- *@str : eafear
+ *_strcmp - finds the lenght of a string
+ *@s1 : eafear
+ *@s2 : 
  *Description : /
  *Return: count
  */
 
-int _len(char *str)
-{
-	int count = 0;
-
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-
-	return (count);
-}
-
-/**
- *_strcmp -
- *@s1 :
- *@s2 :
- *Description : /
- *Return: no return
- */
-
 int _strcmp(char *s1, char *s2)
 {
-	int s1_add = 0;
-	int s2_add = 0;
-	int difference;
-	
-	while (*s1 != '\0')
-    {
-		s1_add = s1_add + *s1;
-		s1++;
-	}
-	while (*s2 != '\0')
-    {
-		s2_add = s2_add + *s1;
-		s2++;
-	}
-	difference = s1_add - s2_add;
+    int i;
 
-	return (difference);
+    for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++);
+    return s1[i] - s2[i];
 }
