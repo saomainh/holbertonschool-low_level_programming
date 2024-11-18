586ccebb3480140a7ea16c2f8aba421dc97877c3
saomainh <enora.nh@gmail.com>
2024-11-18 18:14:48 +0100

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stddef.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *factorial - finds the lenght of a string
+ *@n : eafear
+ * Description : /
+ *Return: count
+ */
+
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        return (-1);
+    }
+    else if (n == 0)
+    {
+        return (1);
+    }
+
+    return (n * factorial(n - 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -2,3 +2,4 @@ int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _putchar(char c);
+int factorial(int n);
