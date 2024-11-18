2c62a59126683273b3aadedd4bb7d7a529ee2508
saomainh <enora.nh@gmail.com>
2024-11-18 17:44:35 +0100

    2 betty doc

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 
 /**
- *_puts_recursion - finds the lenght of a string
- *@s : eafear
- *Description : /
+ *_strlen - finds the lenght of a string
+ *@count : eafear
+ *@s : dzdrer
+ * Description : /
  *Return: count
  */
 
@@ -19,6 +20,14 @@ int _strlen(int count, char *s)
 	count++;
 	return (_strlen(count, s + 1));
 }
+
+/**
+ *_strlen_recursion - finds the lenght of a string
+ *@s : dzdrer
+ * Description : /
+ *Return: count
+ */
+
 int _strlen_recursion(char *s)
 {
 	int count = 0;
