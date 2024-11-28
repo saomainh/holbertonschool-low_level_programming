cae6fcdb3df72602bdf12491cd2a9012625b3f16
saomainh <enora.nh@gmail.com>
2024-11-28 11:22:40 +0100

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "stddef.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+main - finds the lenght of a string
+*@argc : eafear
+*@*argv : jkbkjk
+*Description : /
+*Return: count
+*/
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
