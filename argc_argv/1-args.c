3b0c12e69530526107b1240e3d78b0e4897cd935
saomainh <enora.nh@gmail.com>
2024-11-28 11:26:30 +0100

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "stddef.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - finds the lenght of a string
+*@argc : eafear
+*@*argv : jkbkjk
+*Description : /
+*Return: count
+*/
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
