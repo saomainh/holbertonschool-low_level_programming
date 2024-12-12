2be5f9aff1e9e1417945b695ab35dacdbc43b9b9
saomainh <enora.nh@gmail.com>
2024-12-12 15:33:22 +0100

    0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*create_array - finds the lenght of a string
+*@size : eafear
+*@c : jkbkjk
+*Description : /
+*Return: count
+*/
+
+char *create_array(unsigned int size, char c)
+{
+    char *array;
+    unsigned int i;
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    array = malloc(size * sizeof(char));
+    for (i = 0; i < size; i++)
+    {
+        array[i] = c;
+    }
+    return(array);
+}
