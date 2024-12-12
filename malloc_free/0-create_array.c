0faa0a5d2bd58a10a96d41c323e4b292dc853016
saomainh <enora.nh@gmail.com>
2024-12-12 15:45:41 +0100

    0-2

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -20,14 +20,13 @@ char *create_array(unsigned int size, char c)
         return (NULL);
     }
     array = malloc(size * sizeof(char));
-    for (i = 0; i < size; i++)
-    {
-        array[i] = c;
-    }
     if (array == NULL)
     {
         return (NULL);
     }
-
+    for (i = 0; i < size; i++)
+    {
+        array[i] = c;
+    }
     return(array);
 }
