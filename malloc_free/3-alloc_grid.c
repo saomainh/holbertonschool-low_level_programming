49fe5cc101115fd9c3cf0a6c8b35c44761702a01
saomainh <enora.nh@gmail.com>
2024-12-17 09:42:47 +0100

    3(5)

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
         array[i] = (int *)malloc(width * sizeof(int));
         if (array[i] == NULL)
         {
-            for (k = 0; k > i; k++)
+            for (k = 0; k < i; k++)
             {
                 free(array[k]);
             }
