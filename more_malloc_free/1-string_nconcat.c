1b6bb45aea54455f7f55dab930ca531c10684629
saomainh <enora.nh@gmail.com>
2025-01-07 11:55:31 +0100

    1???

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -38,6 +38,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         return(NULL);
     }
 
+    /*si s1 et s2 sont null return un tableau vide*/
+    if (s1 == NULL && s2 == NULL)
+    {
+        return(array);
+    }
+
+    /*si s1 est null, ajouter directement n 1ers bytes de s2 et return*/
+    if (s1 == NULL)
+    {
+        for (unsigned_i = 0; unsigned_i < n; unsigned_i++)
+        {
+            array[unsigned_i] = s2[unsigned_i];
+            return (array);
+        }
+    }
+
     /*copier s1*/
     for(i = 0; i < size1; i++)
     {
@@ -46,13 +62,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
      /**
     *si n est plus grand ou égal à s2, utiliser tout s2
-    *si n = NULL, s2 est une string vide
+    *si n = 0, ou si size2 est null, return array contenant s1
     */
     if (n > size2)
     {
         n = size2;
     }
-    else if (n == 0)
+    else if (n == 0 || s2 == NULL)
     {
         return (array);
     }
