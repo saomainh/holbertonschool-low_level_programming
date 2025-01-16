6364a4db68b03d926a63697d31a8cd289425d114
saomainh <enora.nh@gmail.com>
2025-01-16 11:03:53 +0100

    remove age from NULL verifying

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,7 +8,29 @@ void print_dog(struct dog *d)
     {
         return;
     }
-    printf("Name: %s\n", d->name);
+    if (d->name == NULL)
+    {
+        printf("Name: (nil)");
+    }
+    else
+    {
+        printf("Name: %s\n", d->name);
+    }
+/*     if (d->age == NULL)
+    {
+        printf("Age: (nil)");
+    }
+    else
+    {
+        printf("Age: %f\n", d->age);
+    } */
     printf("Age: %f\n", d->age);
-    printf("Owner: %s\n", d->owner);
+    if (d->owner == NULL)
+    {
+        printf("Owner: (nil)");
+    }
+    else
+    {
+        printf("Owner: %s\n", d->owner);
+    }
 }
