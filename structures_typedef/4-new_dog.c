50c85014e9b201acdad5f6d9872789c8556af8d0
saomainh <enora.nh@gmail.com>
2025-01-16 13:47:27 +0100

    task 4 strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,13 +1,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new_dog;
-    char *name_cpy = name;
-    char *owner_cpy = owner;
+    char *name_cpy;
+    char *owner_cpy;
+
+    name_cpy = malloc(sizeof(int) * strlen(name));
+    strcpy(name_cpy, name);
+
+    owner_cpy = malloc(sizeof(int) * strlen(owner));
+    strcpy(owner_cpy, owner);
 
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
