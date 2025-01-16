864f6b503da85f84cad22d26102592a0c0905949
saomainh <enora.nh@gmail.com>
2025-01-16 14:21:19 +0100

    task 4 move free

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,19 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
-
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-	
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	return dest;
-}
 
 int _strlen(char *s)
 {
@@ -29,26 +17,38 @@ int _strlen(char *s)
 	return (count);
 }
 
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new_dog;
-    char *name_cpy;
-    char *owner_cpy;
+    char *new_name;
+    char *new_owner;
 
-    name_cpy = malloc(_strlen(name));
-    _strcpy(name_cpy, name);
+    new_name = malloc(_strlen(name));
+    new_owner = malloc(_strlen(owner));
 
-    owner_cpy = malloc(_strlen(owner));
-    _strcpy(owner_cpy, owner);
+    _strcpy(new_name, name);
+    _strcpy(new_owner, owner);
+
+    free(new_name);
+    free(new_owner);
 
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
         return (NULL);
-    new_dog->name = name_cpy;
+    new_dog->name = name;
     new_dog->age = age;
-    new_dog->owner = owner_cpy;
+    new_dog->owner = owner;
     return new_dog;
     free (new_dog);
-    free (name_cpy);
-    free (owner_cpy);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,5 +8,5 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
