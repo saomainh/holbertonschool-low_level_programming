b74727eaa49a8ae8d1604b58c36c7bd5efe7554f
saomainh <enora.nh@gmail.com>
2025-01-16 13:59:59 +0100

    task 4 my own strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,17 +4,28 @@
 #include <string.h>
 #include "dog.h"
 
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new_dog;
     char *name_cpy;
     char *owner_cpy;
 
-    name_cpy = malloc(sizeof(int) * strlen(name));
-    strcpy(name_cpy, name);
+    name_cpy = malloc(strlen(name));
+    _strcpy(name_cpy, name);
 
-    owner_cpy = malloc(sizeof(int) * strlen(owner));
-    strcpy(owner_cpy, owner);
+    owner_cpy = malloc(strlen(owner));
+    _strcpy(owner_cpy, owner);
 
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,3 +8,4 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
