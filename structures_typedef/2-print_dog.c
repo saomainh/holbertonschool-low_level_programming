438b12056b8ed03c36db55bce07fdef3263ddb86
saomainh <enora.nh@gmail.com>
2025-01-16 11:04:47 +0100

    \n

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
     }
     if (d->name == NULL)
     {
-        printf("Name: (nil)");
+        printf("Name: (nil)\n");
     }
     else
     {
@@ -27,7 +27,7 @@ void print_dog(struct dog *d)
     printf("Age: %f\n", d->age);
     if (d->owner == NULL)
     {
-        printf("Owner: (nil)");
+        printf("Owner: (nil)\n");
     }
     else
     {
