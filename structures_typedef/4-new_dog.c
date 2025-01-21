77e835e14de040435276fb889b6b15f1dc53174c
saomainh <enora.nh@gmail.com>
2025-01-21 10:54:55 +0100

    test avec le code de noah

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,60 +1,57 @@
-
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+*string_length-Used to get length of strings
+*@str: String to calculate
+*Return: Integer corresponding
+*/
 
-int _strlen(char *s)
+int string_length(char *str)
 {
-	int i = 0;
-	int count = 0;
-
-	while (s[i] != '\0')
-	{
-		count++;
-		i++;
-	}
-
-	return (count);
+int a = 0;
+while (str[a] != '\0')
+a++;
+return (a);
 }
 
+/**
+ *new_dog-Create a new dog and store a copy of name and owner
+ *@name: name of the doggo
+ *@age: age of the doggo
+ *@owner: Owner of the doggo
+ *Return: dog_t type (dog_t based on function)
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    int name_lenght = _strlen(name) + 1;
-    int owner_lenght = _strlen(owner) + 1;
-    int a;
-    dog_t *new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-
-    /*copy name*/
-    new_dog->name = malloc(name_lenght);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    for (a = 0; a < name_lenght; a++)
-    {
-        new_dog->name[a] = name[a];
-    }
-    new_dog->name[-1] = '\0';
-
-
-    /*copy owner*/
-    new_dog->owner = malloc(owner_lenght);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-    for (a = 0; a < owner_lenght; a++)
-    {
-        new_dog->owner[a] = owner[a];
-    }
-    new_dog->owner[-1] = '\0';
-
-    new_dog->age = age;
-
-    return new_dog;
+int a, name_len, owner_len;
+dog_t *doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
+name_len = string_length(name) + 1;
+doggo->name = malloc(name_len);
+if (doggo->name == NULL)
+{
+free(doggo);
+return (NULL);
+}
+for (a = 0; a < name_len; a++)
+{
+doggo->name[a] = name[a];
+}
+doggo->name[name_len - 1] = '\0';
+owner_len = string_length(owner) + 1;
+doggo->owner = malloc(owner_len);
+if (doggo->owner == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+for (a = 0; a < owner_len; a++)
+{
+doggo->owner[a] = owner[a];
+}
+doggo->owner[owner_len - 1] = '\0';
+doggo->age = age;
+return (doggo);
 }
