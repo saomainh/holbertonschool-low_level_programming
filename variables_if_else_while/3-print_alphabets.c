fb743b6f53e461819694997cd3f3da8a0e8839a5
saomainh <enora.nh@gmail.com>
2025-02-17 10:05:17 +0000

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Short description, single line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+	for (i = 0; i < 26; i++)
+    {
+        putchar('A' + i);
+    }
+	putchar('\n');
+	return (0);
+}
