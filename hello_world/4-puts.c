505f58efc00ac1e2937f50705cd07aff0b81b445
saomainh <enora.nh@gmail.com>
2025-02-17 08:54:09 +0000

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(){
+	char phrase[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(phrase);
+	return (0);
+}
