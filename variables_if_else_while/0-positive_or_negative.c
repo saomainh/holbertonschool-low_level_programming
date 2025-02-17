49e30e30ef93b0d30c1bf6a2917faf7701aad0e7
saomainh <enora.nh@gmail.com>
2025-02-17 09:25:58 +0000

    0 betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-    {
+	{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
@@ -27,7 +27,7 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	else
-    {
+	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
