da41d5ef2e5629a61ef8ada46a0afb58f3b3679b
saomainh <enora.nh@gmail.com>
2025-02-23 16:09:06 +0000

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _abs - Short description, single line
+ * @n : eaoeaijriae
+ * Description: this is a description
+ * Return: Description of the returned value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
