be0a474bcfc1909448149e7df06d4d83335cd9b5
saomainh <enora.nh@gmail.com>
2025-02-23 17:19:48 +0000

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Short description, single line
+ * @n : eaoeaijriae
+ * Description: this is a description
+ * Return: Description of the returned value
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	n = n % 10;
+	_putchar('0' + n);
+	return (n);
+}
