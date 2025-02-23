2ef702bc33a98d7b64fd712ec87f6cab3f185c54
saomainh <enora.nh@gmail.com>
2025-02-23 15:51:08 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _islower - Short description, single line
+ * @c : eaoeaijriae
+ * Description: this is a description
+ * Return: Description of the returned value
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
