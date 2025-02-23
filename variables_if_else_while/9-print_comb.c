4e56b0418f85581de4d0f5b9dbb06a4f5f17909b
saomainh <enora.nh@gmail.com>
2025-02-23 14:48:21 +0000

    9(2)

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,20 +13,16 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+	    putchar('0' + i);
+		if (i < 9)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-			if (i < 9 || j < 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
