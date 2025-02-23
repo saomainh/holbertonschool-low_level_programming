083cb329717b495bb9ff7cf24c7b3b9c3a70f698
saomainh <enora.nh@gmail.com>
2025-02-23 14:34:36 +0000

    8(2

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -14,13 +14,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar('1'+i);
+		putchar('0' + i);
 	}
 	for (i = 0; i < 6; i++)
 	{
-		putchar('a'+i);
+		putchar('a' + i);
 	}
 	putchar('\n');
 	return (0);
