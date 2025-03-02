9dc00c7629a3565f471656eff3bc04a31c051465
saomainh <enora.nh@gmail.com>
2025-03-02 18:39:27 +0000

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Short description, single line
+ * @size: aerfaer
+ * Description: this is a description
+ * Return: Description of the returned value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
