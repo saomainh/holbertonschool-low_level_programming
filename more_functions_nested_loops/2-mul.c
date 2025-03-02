66e851d89a7f4083271c839ba552027e0a115918
saomainh <enora.nh@gmail.com>
2025-03-02 14:49:04 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Short description, single line
+ * @a: aeuca
+ * @b : caeca
+ * Description: this is a description
+ * Return: Description of the returned value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
