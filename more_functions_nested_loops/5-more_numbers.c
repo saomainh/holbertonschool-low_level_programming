a4c64513e8c31fb0b9c7838d5157e3a4ac9f4b1f
saomainh <enora.nh@gmail.com>
2025-03-02 15:22:11 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - Short description, single line
+ * Description: this is a description
+ * Return: Description of the returned value
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int first_digit;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				first_digit = j;
+				while (first_digit >= 10)
+				{
+					first_digit /= 10;
+				}
+				_putchar('0' + first_digit);
+				_putchar('0' + j % 10);
+			}
+			else
+			{
+				_putchar('0' + j);
+			}
+		}
+		_putchar('\n');
+	}
+}
