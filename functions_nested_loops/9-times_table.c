f0f24ee7c718a1de688c64e74e72e88355fe4239
saomainh <enora.nh@gmail.com>
2025-03-06 11:00:35 +0000

    9 putchargit add .git add .

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 				}
 				_putchar('0' + first_digit);
 			}
-		    putchar('0' + (i * j)  % 10);
+			_putchar('0' + (i * j)  % 10);
 			if (j != 9)
 			{
 				_putchar(',');
